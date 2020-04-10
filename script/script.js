1e8a98e62200b690b5ef2e5cfb3953560a7e6678
jdeleonardis <jdeleonardis@hotmail.com>
2020-04-10 09:55:54 -0400

    built todays weather using api, added 5 different uv conditions, removed 'wire frame' from html

--- script/script.js
+++ script/script.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
 
     var inputCity = "";
+    // APIKey
+    var APIKey = "3454e50c3bf841b68c9335f9ed3061d2";  
 
     initDoc();
 
@@ -41,29 +43,128 @@ $(document).ready(function() {
 
         //get here, there should be a city to search with
         //alert(inputCity);
+        //have to call three weather apis.  The current weather doesnt
+        //contain the uv information, but it DOES have the lat/long
+        //needed to call the UV url which DOES have the uv info.  Also,
+        //need to call the 5 day forecast
+
+        //current
+        //var currentWeatherURL = "http://api.openweathermap.org/data/2.5/weather?q=" + inputCity +"&appid=" + APIKey;
+
+        //5 day
+        //var todaysWeatherURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + inputCity + "&appid=" + APIKey;
+
+        //uv
+        // var latValue = 34;
+        // var lonValue = -81.03        
+        // var uvURL = "http://api.openweathermap.org/data/2.5/uvi?appid=" + APIKey + "&lat=" + latValue + "&lon=" + lonValue;
+        
+
+
+        
+        // $.ajax({
+        //     url: currentWeatherURL,
+        //     method: "GET"
+        //   }).then(function(todaysWeather) {
+        //     //   latValue = todaysWeather.coord.lat;
+        //     //   longValue = todaysWeather.coord.lon;
+        //     buildTodaysWeather(todaysWeather);
+        // });
+
+
+
+  
+
+        
+        // $.ajax({
+        //     url: fiveDayWeatherURL,
+        //     method: "GET"
+        //   }).then(function(fiveDaysWeather) {
+        // });
+
         buildTodaysWeather();
         buildFiveDayForecast();
 
     };
 
     //This function builds todays weather - the big section on the right
+    //The function is expecting two things - the response object and the uv data
+    //retrieved from the five day forecast API call.
     function buildTodaysWeather () {
-        $("#todays-weather").empty();
-        var newDiv = $("<div>").addClass("card-body");
-        var newH4 = $("<h4>",{class: "card-title", text: inputCity + " (04/20/2020) "});  //works with a variable for text
-        var newI = $("<i>").addClass("fas fa-sun weathericon");
-        newH4.append(newI);
-        var newP1 = $("<p>",{class: "card-text", text: "Temperature: 91.05 °F"}); //  alt 0 1 7 6
-        var newP2 = $("<p>",{class: "card-text", text: "Humidity: 43%"});
-        var newP3 = $("<p>",{class: "card-text", text: "Wind Speed: 4.7 MPH"});                
-        var newP4 = $("<p>",{class: "card-text", text: "UV Index: "}); 
-        var newSpan = $("<span>",{class: "moderateuv", text: "5.40"});                        
-        newP4.append(newSpan);
-        newDiv.append(newH4, newP1, newP2, newP3, newP4);
-        $("#todays-weather").append(newDiv);
+        var currentWeatherURL = "http://api.openweathermap.org/data/2.5/weather?q=" + inputCity +"&appid=" + APIKey;
+
+        $.ajax({
+            url: currentWeatherURL,
+            method: "GET"
+          }).then(function(todaysWeather) {
+
+            //clear the 'todays-weather' div for new data
+            $("#todays-weather").empty();
+            //create a new card body
+            var newDiv = $("<div>").addClass("card-body");
+            //city name
+            var newH4 = $("<h4>",{class: "card-title", text: inputCity + " (Current) "});  //works with a variable for text
+            //get the icon and appened to the h4
+            var icon =todaysWeather.weather[0].icon;
+            var iconURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
+            var newI = $("<img>").attr("src", iconURL);            
+            newH4.append(newI);
+            
+            //temp, converted from kelvin
+            var tempFromKelvin = (todaysWeather.main.temp - 273.15) * 1.80 + 32
+            var newP1 = $("<p>",{class: "card-text", text: "Temperature: " + tempFromKelvin.toFixed(1) + " °F"}); //  alt 0 1 7 6
+            //humidity
+            var newP2 = $("<p>",{class: "card-text", text: "Humidity: " + todaysWeather.main.humidity +"%"});
+            //wind speed
+            var newP3 = $("<p>",{class: "card-text", text: "Wind Speed: " + todaysWeather.wind.speed + " MPH"});                
+            //uv index
+            var newP4 = $("<p>",{class: "card-text", text: "UV Index: "});
+            
+            //getting the actual uv index is a little more painful.  The current weather api doesnt have the uv - gotta hit a different
+            //api for that using the location coordinates.  Get those from the current weather api and pass them to the uv api.  Then,
+            //build the elements
+            var latValue = todaysWeather.coord.lat;
+            var lonValue = todaysWeather.coord.lon;
+
+            var uvURL = "http://api.openweathermap.org/data/2.5/uvi?appid=" + APIKey + "&lat=" + latValue + "&lon=" + lonValue;
+
+            $.ajax({
+                url: uvURL,
+                method: "GET"
+            }).then(function(uvWeather) {
+                console.log(uvWeather);
+                var uvValue = uvWeather.value;
+                
+                //get the uv colors based on the uv index
+                var uvColor = "";
+                if (uvValue < 3){
+                    uvColor = "lowuv"
+                }
+                else if (uvValue < 6){
+                    uvColor = "mediumuv"                    
+                }
+                else if (uvValue < 8){
+                    uvColor = "highuv"                    
+                }                
+                else if (uvValue < 8){
+                    uvColor = "veryhighuv"                    
+                }                  
+                else {
+                    uvColor = "extremelyhighuv"                    
+                };
+
+                var newSpan = $("<span>",{class: uvColor, text: uvValue});                        
+                newP4.append(newSpan);
+                newDiv.append(newH4, newP1, newP2, newP3, newP4);
+                $("#todays-weather").append(newDiv);
+            });
+        });
+
+
     }
 
     //this function builds the five day forecast
+    //function expects the five day forecast object
     function buildFiveDayForecast () {
         $("#fivedaywords").empty();
         $("#fivedaysection").empty();        
@@ -101,7 +202,7 @@ $(document).ready(function() {
     //get the city that the user input.  return false if there is an empty string, true if text
     //is there
     function getInputCity(){
-        inputCity =  $("#search-input").val();
+        inputCity =  $("#search-input").val().trim();
         if (inputCity == "") {
             alert("Please enter a city to search for.")
             return false;
